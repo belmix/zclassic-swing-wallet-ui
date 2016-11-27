acbcc89502e7861889a8093ae27b9d4309271499
Ivan <ivan@vaklinov.com>
2016-11-27 18:46:49 +0200

    ZClassic GUI wallet initial version

---
+++ src/java/com/vaklinov/zcashui/OSUtil.java
@@ -0,0 +1,221 @@
+/************************************************************************************************
+ *  _________          _     ____          _           __        __    _ _      _   _   _ ___
+ * |__  / ___|__ _ ___| |__ / ___|_      _(_)_ __   __ \ \      / /_ _| | | ___| |_| | | |_ _|
+ *   / / |   / _` / __| '_ \\___ \ \ /\ / / | '_ \ / _` \ \ /\ / / _` | | |/ _ \ __| | | || |
+ *  / /| |__| (_| \__ \ | | |___) \ V  V /| | | | | (_| |\ V  V / (_| | | |  __/ |_| |_| || |
+ * /____\____\__,_|___/_| |_|____/ \_/\_/ |_|_| |_|\__, | \_/\_/ \__,_|_|_|\___|\__|\___/|___|
+ *                                                 |___/
+ *
+ * Copyright (c) 2016 Ivan Vaklinov <ivan@vaklinov.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ **********************************************************************************/
+package com.vaklinov.zcashui;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
+
+
+/**
+ * Utilities - may be OS dependent.
+ *
+ * @author Ivan Vaklinov <ivan@vaklinov.com>
+ */
+public class OSUtil
+{
+
+	public static enum OS_TYPE
+	{
+		LINUX, WINDOWS, MAC_OS, FREE_BSD, OTHER_BSD, SOLARIS, AIX, OTHER_UNIX, OTHER_OS
+	};
+	
+	
+	public boolean isUnixLike(OS_TYPE os)
+	{
+		return os == OS_TYPE.LINUX || os == OS_TYPE.MAC_OS || os == OS_TYPE.FREE_BSD || 
+			   os == OS_TYPE.OTHER_BSD || os == OS_TYPE.SOLARIS || os == OS_TYPE.AIX || 
+			   os == OS_TYPE.OTHER_UNIX;
+	}
+	
+	
+	public boolean isHardUnix(OS_TYPE os)
+	{
+		return os == OS_TYPE.FREE_BSD || 
+			   os == OS_TYPE.OTHER_BSD || os == OS_TYPE.SOLARIS || 
+			   os == OS_TYPE.AIX || os == OS_TYPE.OTHER_UNIX;
+	}
+	
+	
+	public static OS_TYPE getOSType()
+	{
+		String name = System.getProperty("os.name").toLowerCase(Locale.ROOT);
+		
+		if (name.contains("linux"))
+		{
+			return OS_TYPE.LINUX;
+		} else if (name.contains("windows"))
+		{
+			return OS_TYPE.WINDOWS;
+		} else if (name.contains("sunos") || name.contains("solaris"))
+		{
+			return OS_TYPE.SOLARIS;
+		} else if (name.contains("darwin") || name.contains("mac os") || name.contains("macos"))
+		{
+			return OS_TYPE.MAC_OS;
+		} else if (name.contains("free") && name.contains("bsd"))
+		{
+			return OS_TYPE.FREE_BSD;
+		} else if ((name.contains("open") || name.contains("net")) && name.contains("bsd"))
+		{
+			return OS_TYPE.OTHER_BSD;
+		} else if (name.contains("aix"))
+		{
+			return OS_TYPE.AIX;
+		} else if (name.contains("unix"))
+		{
+			return OS_TYPE.OTHER_UNIX;
+		} else
+		{
+			return OS_TYPE.OTHER_OS;
+		}
+	}
+
+
+	public static String getProgramDirectory()
+		throws IOException
+	{
+		// TODO: this way of finding the dir is JAR name dependent - tricky, may not work
+		// if program is repackaged as different JAR!
+		final String JAR_NAME = "ZClassicSwingWalletUI.jar";
+		String cp = System.getProperty("java.class.path");
+		if ((cp != null) && (cp.indexOf(File.pathSeparator) == -1) &&
+			(cp.endsWith(JAR_NAME)))
+		{
+			File pd = new File(cp.substring(0, cp.length() - JAR_NAME.length()));
+
+			if (pd.exists() && pd.isDirectory())
+			{
+				return pd.getCanonicalPath();
+			}
+		}
+
+		String userDir = System.getProperty("user.dir");
+		if (userDir != null)
+		{
+			File ud = new File(userDir);
+
+			if (ud.exists() && ud.isDirectory())
+			{
+				return ud.getCanonicalPath();
+			}
+		}
+
+		// TODO: tests and more options
+
+		return new File(".").getCanonicalPath();
+	}
+
+
+	public static String getBlockchainDirectory()
+		throws IOException
+	{
+		OS_TYPE os = getOSType();
+		
+		if (os == OS_TYPE.MAC_OS)
+		{
+			return new File(System.getProperty("user.home") + "/Library/Application Support/zclassic").getCanonicalPath();
+		} else
+		{
+			return new File(System.getProperty("user.home") + "/.zclassic").getCanonicalPath();
+		}
+	}
+
+
+	// Directory with program settings to store
+	public static String getSettingsDirectory()
+		throws IOException
+	{
+		File dir = new File(System.getProperty("user.home") + "/.ZClassicSwingWalletUI");
+		if (!dir.exists())
+		{
+			dir.mkdirs();
+		}
+
+		return dir.getCanonicalPath();
+	}
+
+
+	public static String getSystemInfo()
+		throws IOException, InterruptedException
+	{
+		OS_TYPE os = getOSType();
+		
+		if (os == OS_TYPE.MAC_OS)
+		{
+			CommandExecutor uname = new CommandExecutor(new String[] { "uname", "-sr" });
+		    return uname.execute() + "; " + 
+		           System.getProperty("os.name") + " " + System.getProperty("os.version");
+		} else
+		{
+			CommandExecutor uname = new CommandExecutor(new String[] { "uname", "-srv" });
+		    return uname.execute();
+		}
+	}
+
+
+	// Can be used to find zcashd/zcash-cli
+	// Null if not found
+	public static File findZCashCommand(String command)
+		throws IOException
+	{
+		final String dirs[] = new String[]
+		{
+			"/usr/bin/", // Typical Ubuntu
+			"/bin/",
+			"/usr/local/bin/",
+			"/usr/local/zclassic/bin/",
+			"/usr/lib/zclassic/bin/",
+			"/opt/local/bin/",
+			"/opt/local/zclassic/bin/",
+			"/opt/zclassic/bin/"
+		};
+
+		for (String d : dirs)
+		{
+			File f = new File(d + command);
+			if (f.exists())
+			{
+				return f;
+			}
+		}
+		
+		// Try in the current directory
+		File f = new File("." + File.separator + command);
+		if (f.exists() && f.isFile())
+		{
+			return f.getCanonicalFile();
+		}
+
+		// TODO: Try to find it with which/PATH
+		
+		return null;
+	}
+}
---
+++ src/java/com/vaklinov/zcashui/ZCashUI.java
@@ -0,0 +1,386 @@
+/************************************************************************************************
+ *  _________          _     ____          _           __        __    _ _      _   _   _ ___
+ * |__  / ___|__ _ ___| |__ / ___|_      _(_)_ __   __ \ \      / /_ _| | | ___| |_| | | |_ _|
+ *   / / |   / _` / __| '_ \\___ \ \ /\ / / | '_ \ / _` \ \ /\ / / _` | | |/ _ \ __| | | || |
+ *  / /| |__| (_| \__ \ | | |___) \ V  V /| | | | | (_| |\ V  V / (_| | | |  __/ |_| |_| || |
+ * /____\____\__,_|___/_| |_|____/ \_/\_/ |_|_| |_|\__, | \_/\_/ \__,_|_|_|\___|\__|\___/|___|
+ *                                                 |___/
+ *
+ * Copyright (c) 2016 Ivan Vaklinov <ivan@vaklinov.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ **********************************************************************************/
+package com.vaklinov.zcashui;
+
+
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JTabbedPane;
+import javax.swing.KeyStroke;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import com.vaklinov.zcashui.ZCashClientCaller.WalletCallException;
+import com.vaklinov.zcashui.ZCashInstallationObserver.InstallationDetectionException;
+
+
+/**
+ * Main ZClassic Window.
+ *
+ * @author Ivan Vaklinov <ivan@vaklinov.com>
+ */
+public class ZCashUI
+    extends JFrame
+{
+    private ZCashInstallationObserver installationObserver;
+    private ZCashClientCaller clientCaller;
+    private StatusUpdateErrorReporter errorReporter;
+
+    private WalletOperations walletOps;
+
+    private JMenuItem menuItemExit;
+    private JMenuItem menuItemAbout;
+    private JMenuItem menuItemEncrypt;
+    private JMenuItem menuItemBackup;
+    private JMenuItem menuItemExportKeys;
+    private JMenuItem menuItemImportKeys;
+    private JMenuItem menuItemShowPrivateKey;
+
+    private DashboardPanel dashboard;
+    private AddressesPanel addresses;
+    private SendCashPanel  sendPanel;
+
+    public ZCashUI()
+        throws IOException, InterruptedException, WalletCallException
+    {
+        super("ZClassic Swing Wallet UI 0.06 (beta)");
+        ClassLoader cl = this.getClass().getClassLoader();
+
+        this.setIconImage(new ImageIcon(cl.getResource("images/Z-yellow.orange-logo.png")).getImage());
+
+        Container contentPane = this.getContentPane();
+
+        errorReporter = new StatusUpdateErrorReporter(this);
+        installationObserver = new ZCashInstallationObserver(OSUtil.getProgramDirectory());
+        clientCaller = new ZCashClientCaller(OSUtil.getProgramDirectory());
+
+        // Build content
+        JTabbedPane tabs = new JTabbedPane();
+        Font oldTabFont = tabs.getFont();
+        Font newTabFont  = new Font(oldTabFont.getName(), Font.BOLD | Font.ITALIC, oldTabFont.getSize() * 57 / 50);
+        tabs.setFont(newTabFont);
+        tabs.addTab("Overview ",
+        		    new ImageIcon(cl.getResource("images/overview.png")),
+        		    dashboard = new DashboardPanel(this, installationObserver, clientCaller, errorReporter));
+        tabs.addTab("Own addresses ",
+        		    new ImageIcon(cl.getResource("images/address-book.png")),
+        		    addresses = new AddressesPanel(clientCaller, errorReporter));
+        tabs.addTab("Send cash ",
+        		    new ImageIcon(cl.getResource("images/send.png")),
+        		    sendPanel = new SendCashPanel(clientCaller, errorReporter));
+        contentPane.add(tabs);
+
+        this.walletOps = new WalletOperations(
+            	this, tabs, dashboard, addresses, sendPanel, installationObserver, clientCaller, errorReporter);
+
+        this.setSize(new Dimension(870, 427));
+
+        // Build menu
+        JMenuBar mb = new JMenuBar();
+        JMenu file = new JMenu("Main");
+        file.setMnemonic(KeyEvent.VK_M);
+        int accelaratorKeyMask = Toolkit.getDefaultToolkit ().getMenuShortcutKeyMask();
+        file.add(menuItemAbout = new JMenuItem("About...", KeyEvent.VK_A));
+        menuItemAbout.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, accelaratorKeyMask));
+        file.addSeparator();
+        file.add(menuItemExit = new JMenuItem("Quit", KeyEvent.VK_Q));
+        menuItemExit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, accelaratorKeyMask));
+        mb.add(file);
+
+        JMenu wallet = new JMenu("Wallet");
+        wallet.setMnemonic(KeyEvent.VK_W);
+        wallet.add(menuItemBackup = new JMenuItem("Backup...", KeyEvent.VK_B));
+        menuItemBackup.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_B, accelaratorKeyMask));
+        wallet.add(menuItemEncrypt = new JMenuItem("Encrypt...", KeyEvent.VK_E));
+        menuItemEncrypt.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, accelaratorKeyMask));
+        wallet.add(menuItemExportKeys = new JMenuItem("Export private keys...", KeyEvent.VK_K));
+        menuItemExportKeys.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_K, accelaratorKeyMask));
+        wallet.add(menuItemImportKeys = new JMenuItem("Import private keys...", KeyEvent.VK_I));
+        menuItemImportKeys.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_I, accelaratorKeyMask));
+        wallet.add(menuItemShowPrivateKey = new JMenuItem("Show private key...", KeyEvent.VK_P));
+        menuItemShowPrivateKey.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_P, accelaratorKeyMask));
+        
+        mb.add(wallet);
+
+        // TODO: Temporarily disable encryption until further notice - Oct 24 2016
+        menuItemEncrypt.setEnabled(false);
+                        
+        this.setJMenuBar(mb);
+
+        // Add listeners etc.
+        menuItemExit.addActionListener(
+            new ActionListener()
+            {
+                @Override
+                public void actionPerformed(ActionEvent e)
+                {
+                    ZCashUI.this.exitProgram();
+                }
+            }
+        );
+
+        menuItemAbout.addActionListener(
+            new ActionListener()
+            {
+                @Override
+                public void actionPerformed(ActionEvent e)
+                {
+                	try
+                	{
+                		AboutDialog ad = new AboutDialog(ZCashUI.this);
+                		ad.setVisible(true);
+                	} catch (UnsupportedEncodingException uee)
+                	{
+                		uee.printStackTrace();
+                		ZCashUI.this.errorReporter.reportError(uee);
+                	}
+                }
+            }
+        );
+
+        menuItemBackup.addActionListener(   
+        	new ActionListener()
+            {
+                @Override
+                public void actionPerformed(ActionEvent e)
+                {
+                    ZCashUI.this.walletOps.backupWallet();
+                }
+            }
+        );
+        
+        menuItemEncrypt.addActionListener(
+            new ActionListener()
+            {
+                @Override
+                public void actionPerformed(ActionEvent e)
+                {
+                    ZCashUI.this.walletOps.encryptWallet();
+                }
+            }
+        );
+
+        menuItemExportKeys.addActionListener(   
+            new ActionListener()
+            {
+                @Override
+                public void actionPerformed(ActionEvent e)
+                {
+                    ZCashUI.this.walletOps.exportWalletPrivateKeys();
+                }
+            }
+       );
+        
+       menuItemImportKeys.addActionListener(   
+            new ActionListener()
+            {
+                @Override
+                public void actionPerformed(ActionEvent e)
+                {
+                    ZCashUI.this.walletOps.importWalletPrivateKeys();
+                }
+            }
+       );
+       
+       menuItemShowPrivateKey.addActionListener(   
+            new ActionListener()
+            {
+                @Override
+                public void actionPerformed(ActionEvent e)
+                {
+                    ZCashUI.this.walletOps.showPrivateKey();
+                }
+            }
+       );
+
+        // Close operation
+        this.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        this.addWindowListener(new WindowAdapter()
+        {
+            @Override
+            public void windowClosing(WindowEvent e)
+            {
+                ZCashUI.this.exitProgram();
+            }
+        });
+
+        // Show initial message
+        SwingUtilities.invokeLater(new Runnable()
+        {
+            public void run()
+            {
+                try
+                {
+                    String userDir = OSUtil.getSettingsDirectory();
+                    File warningFlagFile = new File(userDir + "/initialInfoShown.flag");
+                    if (warningFlagFile.exists())
+                    {
+                        return;
+                    } else
+                    {
+                        warningFlagFile.createNewFile();
+                    }
+
+                } catch (IOException ioe)
+                {
+                    /* TODO: report exceptions to the user */
+                    ioe.printStackTrace();
+                }
+
+                JOptionPane.showMessageDialog(
+                    ZCashUI.this.getRootPane().getParent(),
+                    "The ZClassic GUI Wallet is currently considered experimental. Use of this software\n" +
+                    "comes at your own risk! \n" +
+                    " \n" +
+                    "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n" +
+                    "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n" +
+                    "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n" +
+                    "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n" +
+                    "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n" +
+                    "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN\n" +
+                    "THE SOFTWARE.\n\n" +
+                    "(This message will be shown only once)",
+                    "Disclaimer", JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+    }
+
+    public void exitProgram()
+    {
+        System.out.println("Exiting ...");
+
+        this.dashboard.stopThreadsAndTimers();
+
+        ZCashUI.this.setVisible(false);
+        ZCashUI.this.dispose();
+
+        System.exit(0);
+    }
+
+    public static void main(String argv[])
+        throws IOException
+    {
+        try
+        {
+            System.out.println("Starting ZClassic Swing Wallet ...");
+            System.out.println("OS: " + System.getProperty("os.name") + " = " + OSUtil.getOSType());
+            System.out.println("Current directory: " + new File(".").getCanonicalPath());
+            System.out.println("Class path: " + System.getProperty("java.class.path"));
+            System.out.println("Environment PATH: " + System.getenv("PATH"));
+
+            ////////////////////////////////////////////////////////////
+            for (LookAndFeelInfo ui : UIManager.getInstalledLookAndFeels())
+            {
+                System.out.println("Available look and feel: " + ui.getName() + " " + ui.getClassName());
+                if (ui.getName().equals("Nimbus"))
+                {
+                    UIManager.setLookAndFeel(ui.getClassName());
+                    break;
+                }
+            }
+
+            /////////////////////////////////////////////////////
+            ZCashUI ui = new ZCashUI();
+            ui.setVisible(true);
+
+        } catch (InstallationDetectionException ide)
+        {
+            ide.printStackTrace();
+            JOptionPane.showMessageDialog(
+                null,
+                "This program was started in directory: " + OSUtil.getProgramDirectory() + "\n" +
+                ide.getMessage() + "\n" +
+                "See the console output for more detailed error information!",
+                "Installation error",
+                JOptionPane.ERROR_MESSAGE);
+            System.exit(1);
+        } catch (WalletCallException wce)
+        {
+            wce.printStackTrace();
+
+            if ((wce.getMessage().indexOf("{\"code\":-28,\"message\":\"Verifying blocks")      != -1)  ||
+            	(wce.getMessage().indexOf("{\"code\":-28,\"message\":\"Rescanning")            != -1)  ||
+            	(wce.getMessage().indexOf("{\"code\":-28,\"message\":\"Loading wallet")        != -1)  ||
+            	(wce.getMessage().indexOf("{\"code\":-28,\"message\":\"Activating best chain") != -1)  ||
+            	(wce.getMessage().indexOf("{\"code\":-28,\"message\":\"Loading addresses")     != -1))
+            {
+                JOptionPane.showMessageDialog(
+                        null,
+                        "It appears that zcashd has been started but is not ready to accept wallet\n" +
+                        "connections. It is still loading the wallet and blockchain. Please try to \n" +
+                        "start the GUI wallet later...",
+                        "Wallet communication error",
+                        JOptionPane.ERROR_MESSAGE);
+            } else
+            {
+                JOptionPane.showMessageDialog(
+                    null,
+                    "There was a problem communicating with the ZClassic daemon/wallet. \n" +
+                    "Please ensure that the ZClassic server zcashd is started (e.g. via \n" + 
+                    "command  \"zcashd --daemon\"). Error message is: \n" +
+                     wce.getMessage() +
+                    "See the console output for more detailed error information!",
+                    "Wallet communication error",
+                    JOptionPane.ERROR_MESSAGE);
+            }
+
+            System.exit(2);
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(
+                null,
+                "A general unexpected critical error has occurred: \n" + e.getMessage() + "\n" +
+                "See the console output for more detailed error information!",
+                "Error",
+                JOptionPane.ERROR_MESSAGE);
+            System.exit(3);
+        }
+    }
+}
